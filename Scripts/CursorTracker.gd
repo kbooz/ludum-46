9edb9647ad74f77ea805ba21dec1c20611023443
Diogoamorim27 <diogoamorim27@gmail.com>
2020-04-18 12:35:15 -0300

    Started making ui interactions

---
+++ Scripts/CursorTracker.gd
@@ -0,0 +1,4 @@
+extends Area2D
+
+func _process(_delta):
+	position = get_viewport().get_mouse_position()
---
+++ Scripts/ResourceProp.gd
@@ -0,0 +1,44 @@
+extends Area2D
+
+onready var outline : = $Outline
+
+var game_manager
+
+var is_hovering : = false
+var interactive : = false
+
+func _ready():
+	outline.visible = false
+	game_manager = get_tree().get_nodes_in_group("Game Manager")[0]
+
+func _process(_delta):
+	if is_hovering:
+		outline.visible = true
+	else:
+		outline.visible = false
+
+func _input(event):
+	if (event is InputEventMouseButton) and event.is_pressed():
+		if is_hovering and interactive:
+			pass
+
+func _on_ResourceProp_area_entered(area):
+	if area.name == "CursorTracker":
+		is_hovering = true
+		
+
+func _on_ResourceProp_area_exited(area):
+	if area.name == "CursorTracker":
+		is_hovering = false
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		interactive = true
+	pass # Replace with function body.
+
+
+func _on_Area2D_body_exited(body):
+	if body.name == "Player":
+		interactive = false
+	pass # Replace with function body.
