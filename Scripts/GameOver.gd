0b606488be4d7fe2c353a9f66e90562695a26cfb
Diogoamorim27 <diogoamorim27@gmail.com>
2020-04-20 15:20:48 -0300

    Game over 'score screen' added

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -1,5 +1,14 @@
 extends Control
 
+func _ready():
+	var string = "Nice Job!\n\n You kept the party going for %d:%d!\n\n%d people attended!\n\nYou got to level %d!"
+	$Panel/Label.text =  string % [GlobalResources.time_mins, GlobalResources.time_secs, GlobalResources.max_attendance, GlobalResources.level_reached]
+
 func _on_Button_pressed():
 	get_tree().change_scene("res://Test Scenes/LevelDesignTest.tscn")
+	pass # Replace with function body.
+
+
+func _on_Button3_pressed():
+	get_tree().change_scene("res://Test Scenes/Scenes/StartScreen.tscn")
 	pass # Replace with function body.
--- Scripts/GlobalResources.gd
+++ Scripts/GlobalResources.gd
@@ -1,3 +1,8 @@
 extends Node
 
 enum RESOURCES {EMPTY, BEER, FOOD, MUSIC, CLEANING}
+
+var time_secs : int = 0
+var time_mins : int = 0
+var max_attendance = 0
+var level_reached = 0
